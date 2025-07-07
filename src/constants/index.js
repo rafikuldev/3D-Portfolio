ac231813550bfd6e0f58c8d36c37670d918c4f35
rafikulislamrafi <mohammadrafe0099@gmail.com>
2025-07-08 02:39:21 +0600

    link

--- src/constants/index.js
+++ src/constants/index.js
@@ -224,19 +224,19 @@ export const myProjects = [
 ];
 
 export const mySocials = [
-  {
-    name: "WhatsApp",
-    href: "",
-    icon: "/assets/socials/whatsApp.svg",
-  },
+  // {
+  //   name: "WhatsApp",
+  //   href: "",
+  //   icon: "/assets/socials/whatsApp.svg",
+  // },
   {
     name: "Linkedin",
-    href: "https://www.linkedin.com/in/ali-sanati/",
+    href: "https://www.linkedin.com/in/rafikuldev/",
     icon: "/assets/socials/linkedIn.svg",
   },
   {
     name: "Instagram",
-    href: "https://www.instagram.com/ali.sanatidev/reels/",
+    href: "https://www.instagram.com/rafikuldev/",
     icon: "/assets/socials/instagram.svg",
   },
 ];
